70a1e843b0791e91e9225f3e9d052d2c8941021c
Fabian <fgambinoC@gmail.com>
2019-09-06 22:29:28 -0400

    added admin panel code and article objects into database

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add in thumbnail later
+    # add in author later
+
+    def __str__(self):
+        return self.title
