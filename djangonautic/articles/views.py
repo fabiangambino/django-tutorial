c8f3afad9e6dd25abf5ff8d41a27d389c8317b5f
Fabian <fgambinoC@gmail.com>
2019-09-14 11:26:18 -0400

    final commit

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def article_list(request):
@@ -18,4 +19,13 @@ def article_detail(request, slug):
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
-    return render(request, "articles/article_create.html")
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, "articles/article_create.html", {'form': form})
